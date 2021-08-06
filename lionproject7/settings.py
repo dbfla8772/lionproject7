1690843cc7e95be43e12b909befc8ea8d416ac4a
dbfla8772 <dbfla5527@naver.com>
2021-08-06 17:06:32 +0900

    앱 재사용, 메일, 쪽지 기능 구현

--- lionproject7/settings.py
+++ lionproject7/settings.py
@@ -145,7 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dbfla87723@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbfla6069@'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
